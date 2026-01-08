6452c9502de85d754c8b3ea55083a14e5e34a1a8
yadhvi13 <https://github.com/yadhvi13>
2026-01-08 23:25:49 +0530

    PORTFOLIo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,72 +1,4 @@
-// import React from "react";
-// import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaYoutube } from "react-icons/fa";
 
-// const Footer = () => {
-//   // Smooth scroll function
-//   const handleScroll = (sectionId) => {
-//     const section = document.getElementById(sectionId);
-//     if (section) {
-//       section.scrollIntoView({ behavior: "smooth" });
-//     }
-//   };
-
-//   return (
-//     <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
-//       <div className="container mx-auto text-center">
-//         {/* Name / Logo */}
-//         <h2 className="text-xl font-semibold text-purple-500">Tarun Kaushik</h2>
-
-//         {/* Navigation Links - Responsive */}
-//         <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
-//           {[
-//             { name: "About", id: "about" },
-//             { name: "Skills", id: "skills" },
-//             { name: "Experience", id: "experience" },
-//             { name: "Projects", id: "projects" },
-//             { name: "Education", id: "education" },
-//           ].map((item, index) => (
-//             <button
-//               key={index}
-//               onClick={() => handleScroll(item.id)}
-//               className="hover:text-purple-500 text-sm sm:text-base my-1"
-//             >
-//               {item.name}
-//             </button>
-//           ))}
-//         </nav>
-
-//         {/* Social Media Icons - Responsive */}
-//         <div className="flex flex-wrap justify-center space-x-4 mt-6">
-//           {[
-//             { icon: <FaFacebook />, link: "https://www.facebook.com/tarun.kaushik.3511041/" },
-//             { icon: <FaTwitter />, link: "https://twitter.com/CodingMaster6?s=09" },
-//             { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/tarun-kaushik-553b441a4" },
-//             { icon: <FaInstagram />, link: "https://www.instagram.com/coding_.master/" },
-//             { icon: <FaYoutube />, link: "https://www.youtube.com/codingmasteryt" },
-            
-//           ].map((item, index) => (
-//             <a
-//               key={index}
-//               href={item.link}
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
-//             >
-//               {item.icon}
-//             </a>
-//           ))}
-//         </div>
-
-//         {/* Copyright Text */}
-//         <p className="text-sm text-gray-400 mt-6">
-//           © 2025 Tarun Kaushik. All rights reserved.
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 
 
 import React from "react";
@@ -134,7 +66,7 @@ const Footer = () => {
               { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/yogeeta-752388331/" },
               { icon: <FaGithub />, link: "https://github.com/yadhvi13" },
               { icon: <FaTwitter />, link: "https://twitter.com/" },
-              { icon: <FaYoutube />, link: "https://www.youtube.com/codingmasteryt" },
+              { icon: <FaYoutube />, link: "https://www.youtube.com" },
             ].map((item, i) => (
               <motion.a
                 key={i}
